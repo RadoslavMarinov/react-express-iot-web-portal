3fcbf10f4332613f7f24ae8e770c84e74198b574
Radoslav <rikotech@abv.bg>
2019-01-05 20:03:19 +0200

    .

--- client/src/components/endpoints/BinarySwitch.jsx
+++ client/src/components/endpoints/BinarySwitch.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class BinarySwitch extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 1 };
+    this.state = { level: 1, disabled = false };
     this.level = 1;
   }
 
@@ -13,6 +13,9 @@ class BinarySwitch extends Component {
       thus for 'key' we will have something like 
       this: bs1s
     */
+
+    this.setState({disabled:true});
+
     var key = swName + "s"; //suffix 's' stands for "set" command
     console.log(`Name: ${swName}; Device ID: ${this.props.dev.id}`);
     this.level === 1 ? (this.level = 0) : (this.level = 1);
@@ -28,9 +31,10 @@ class BinarySwitch extends Component {
         x.hasOwnProperty(this.props.name)
       );
       console.log(ep[this.props.name].state);
-      this.setState({ level: ep[this.props.name].state });
+      this.setState({ level: ep[this.props.name].state, disabled: false });
     } else {
       console.log(resJson.status, resJson.message);
+      this.setState({disabled:false});
     }
 
     //Find endpoint:
@@ -39,7 +43,8 @@ class BinarySwitch extends Component {
   render() {
     return (
       <button onClick={this.onClick} name={this.props.name}>
-        {this.props.displayName + " " + this.state.level}
+        {this.props.displayName + " " + this.state.level} disabled=
+        {this.state.disabled}
       </button>
     );
   }
